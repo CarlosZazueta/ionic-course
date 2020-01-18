2cf7f3b3f3cbe894ffc7c13301858103feb24e60
Carlos Clemente <carlosc_zg98@hotmail.com>
2020-01-17 23:07:50 -0700

    Cambios guardados AppComponent

--- angular-refresher/src/app/app.component.ts
+++ angular-refresher/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  persons: string[] = ['Max', 'Manuel', 'Anna'];
+  persons: string[];
+
+  constructor() {
+    this.persons = ['Max', 'Manuel', 'Anna'];
+  }
 }
