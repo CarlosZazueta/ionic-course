d73afd65a1ecdaa892892942da77a528194f26d2
Carlos Clemente <carlosc_zg98@hotmail.com>
2020-01-20 18:35:02 -0700

    Sunscribe function, Observables.

--- angular-refresher/src/app/persons/persons.service.ts
+++ angular-refresher/src/app/persons/persons.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Subject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PersonsService {
-  public persons = ['Max', 'Manuel', 'Anna'];
+  personsChanged = new Subject<string[]>();
+  public persons: string[] = ['Max', 'Manuel', 'Anna'];
 
   public addPerson(name: string) {
     this.persons.push(name);
-    console.table(this.persons);
+    this.personsChanged.next(this.persons);
+  }
+  public removePerson(name: string){
+    this.persons = this.persons.filter(person => {
+      return person !== name;
+    });
+    this.personsChanged.next(this.persons);
   }
 }
